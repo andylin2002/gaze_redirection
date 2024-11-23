e0f54ac9c2510ffacb0465f7388b3ac0275eedd0
andylin2002 <andytw2002@gmail.com>
2024-11-23 23:06:34 +0800

    run face_detection successfully

---
+++ test.py
@@ -0,0 +1,12 @@
+# 把一些警告的訊息暫時関掉
+import warnings
+warnings.filterwarnings('ignore')
+
+# Utilities相關函式庫
+import os
+import numpy as np
+
+# 圖像處理/展現的相關函式庫
+import dlib
+from PIL import Image, ImageDraw, ImageFont
+import matplotlib.pyplot as plt
