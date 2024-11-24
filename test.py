224cea40f1dd28218658d659884653bc36d4a450
andylin2002 <andytw2002@gmail.com>
2024-11-24 13:22:33 +0800

    add folder: all/ in eyespatch_dataset

--- test.py
+++ test.py
@@ -1,32 +1,28 @@
 import os
+import shutil
 
-# eyespatch 資料夾的路徑
+# 設定 eyespatch_dataset 資料夾的路徑
+dataset_path = "/home/andy/AILab/AIfinal/gaze_redirection-master/eyespatch_dataset"  # 替換成你的實際路徑
+all_folder = os.path.join(dataset_path, "all")
 
-subfolder_path = '/home/andy/AILab/AIfinal/gaze_redirection-master/eyespatch_dataset/30P'
-    
-# 遍歷子資料夾中的所有檔案
-for filename in os.listdir(subfolder_path):
-    # 檢查檔案是否以 "left" 或 "right" 開頭
-    if filename.startswith("left_") or filename.startswith("right_"):
-        # 分離檔案名稱和副檔名
-        name_parts = filename.split("_")
-        prefix = name_parts[0]
-        rest = "_".join(name_parts[1:])  # 取得剩餘部分
-        
-        # 根據檔案開頭選擇新後綴
-        if prefix == "left":
-            new_name = rest.replace(".jpg", "_L.jpg")
-        elif prefix == "right":
-            new_name = rest.replace(".jpg", "_R.jpg")
-        else:
-            continue  # 如果不是 left 或 right，跳過
-        
-        # 完整的舊路徑和新路徑
-        old_path = os.path.join(subfolder_path, filename)
-        new_path = os.path.join(subfolder_path, new_name)
-        
-        # 重新命名檔案
-        os.rename(old_path, new_path)
-        print(f"Renamed: {old_path} -> {new_path}")
+# 創建新的資料夾 "all"
+if not os.path.exists(all_folder):
+    os.makedirs(all_folder)
 
-print("Renaming completed!")
+# 取得子資料夾列表
+subfolders = ["N30P", "N15P", "0P", "15P", "30P"]
+
+# 複製每個子資料夾的內容到 "all" 資料夾
+for subfolder in subfolders:
+    subfolder_path = os.path.join(dataset_path, subfolder)
+    if os.path.exists(subfolder_path):  # 確保子資料夾存在
+        for file_name in os.listdir(subfolder_path):  # 遍歷子資料夾中的所有檔案
+            src_file = os.path.join(subfolder_path, file_name)
+            dest_file = os.path.join(all_folder, file_name)
+            if os.path.isfile(src_file):  # 確保是檔案而非資料夾
+                shutil.copy(src_file, dest_file)  # 複製檔案
+            elif os.path.isdir(src_file):  # 若遇到子資料夾也可以選擇是否處理
+                print(f"Skipping directory {src_file}")
+
+print("All files copied to the 'all' folder.")
+
