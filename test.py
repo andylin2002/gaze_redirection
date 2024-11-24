c73d4abcca569d817c7218dbdc77969df549826b
andylin2002 <andytw2002@gmail.com>
2024-11-24 12:41:45 +0800

    rename file to '_L' or '_R'

--- test.py
+++ test.py
@@ -1,12 +1,32 @@
-# 把一些警告的訊息暫時関掉
-import warnings
-warnings.filterwarnings('ignore')
-
-# Utilities相關函式庫
 import os
-import numpy as np
 
-# 圖像處理/展現的相關函式庫
-import dlib
-from PIL import Image, ImageDraw, ImageFont
-import matplotlib.pyplot as plt
+# eyespatch 資料夾的路徑
+
+subfolder_path = '/home/andy/AILab/AIfinal/gaze_redirection-master/eyespatch_dataset/30P'
+    
+# 遍歷子資料夾中的所有檔案
+for filename in os.listdir(subfolder_path):
+    # 檢查檔案是否以 "left" 或 "right" 開頭
+    if filename.startswith("left_") or filename.startswith("right_"):
+        # 分離檔案名稱和副檔名
+        name_parts = filename.split("_")
+        prefix = name_parts[0]
+        rest = "_".join(name_parts[1:])  # 取得剩餘部分
+        
+        # 根據檔案開頭選擇新後綴
+        if prefix == "left":
+            new_name = rest.replace(".jpg", "_L.jpg")
+        elif prefix == "right":
+            new_name = rest.replace(".jpg", "_R.jpg")
+        else:
+            continue  # 如果不是 left 或 right，跳過
+        
+        # 完整的舊路徑和新路徑
+        old_path = os.path.join(subfolder_path, filename)
+        new_path = os.path.join(subfolder_path, new_name)
+        
+        # 重新命名檔案
+        os.rename(old_path, new_path)
+        print(f"Renamed: {old_path} -> {new_path}")
+
+print("Renaming completed!")
